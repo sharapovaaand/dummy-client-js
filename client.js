2892bb4394b9abdcb10ce0dc4760b56b6602fc70
asharapova <sharapovaaand@gmail.com>
2023-05-17 00:05:02 +0400

    Initial commit

---
+++ client.js
@@ -0,0 +1,121 @@
+const querystring = require("querystring");
+require('dotenv').config();
+const newProducts = require("./new_products");
+const testEndpoints = require("./endpoints");
+
+class dummyApiCallerClass {
+
+	async request(endpoint = "", options = {}) {
+		let url = testEndpoints.root + endpoint;
+		let headers = {
+    	'Content-type': 'application/json'
+  	};
+
+  	let res = await fetch(url, options);
+  	if (res.status == 200) {
+    	let json = await res.json(); // (3)
+    return json;
+  	}
+
+  	throw new Error(res.status);
+	};
+
+	getCategory(categoryName, auth = false) {
+	  let url = testEndpoints.categories + categoryName;
+	  let config = {
+	   method: 'GET'
+	  }
+	  if (auth == true) {
+	  	config.headers.Authorization = 'Bearer ' + process.env.TOKEN;
+	  }
+	  return this.request(url, config);
+  };
+
+	getProduct(id, auth = false) {
+		let url = testEndpoints.signle_product + id;
+		let config = {
+	    method: 'GET'
+	  }
+	  if (auth == true) {
+	  	config['headers']= {"Authorization" : 'Bearer ' + process.env.TOKEN};
+	  }
+	  return this.request(url, config);
+	};
+
+	addProduct (body, auth = false) {
+		let url = testEndpoints.add_product;
+		let config = {
+	    method: 'POST',
+	    headers: { 'Content-Type': 'application/json' },
+	    body: JSON.stringify(body)
+	  };
+	  if (auth == true)
+	  {
+	  	config.headers.Authorization = 'Bearer ' + process.env.TOKEN;
+	  }
+	  return this.request(url, config);
+	}
+
+	deleteProduct (id, auth = false) {
+		let url = testEndpoints.signle_product + id;
+		let config = {
+	    method: 'DELETE',
+	  }
+	  if (auth == true)
+	  {
+	  	config['headers']= {"Authorization" : 'Bearer ' + process.env.TOKEN};
+	  }
+	  return this.request(url, config);
+	};
+
+};
+
+
+// Examples
+
+async function clientExample() {
+
+	const dummyApiCaller = new dummyApiCallerClass();
+
+	let product;
+
+	// Get the first five products
+	console.log(`\n`);
+	console.log('Get the first five products');
+
+	for (i = 0; i < 4; i++)
+	{
+		product = await dummyApiCaller.getProduct(i+1);
+		console.log(product);
+		console.log(`\n`);
+	}
+
+
+	//Add a new product from a file
+	console.log(`\n`);
+	console.log('Add a new product from a file');
+
+	for (i = 0; i < newProducts.products.length; i++) {
+	  product = await dummyApiCaller.addProduct(newProducts.products[i])
+		console.log(product);
+		console.log(`\n`);
+	}
+
+
+	//Delete the first five products
+	console.log(`\n`);
+	console.log('Delete the first five products');
+
+	for (i = 0; i < 4; i++)
+	{
+		product = await dummyApiCaller.deleteProduct(i+1);
+		console.log(product);
+		console.log(`\n`);
+	}
+} 
+
+clientExample();
+
+
+
+
